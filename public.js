e69a20e05513a0dec145c1ee9bc071bc5f789f3f
jingxc <jingxiaochong@HUAWEI-MacBook-Pro.local>
2023-12-08 13:22:31 +0800

    wancheng

--- public.js
+++ public.js
@@ -1,6 +1,6 @@
 //活动域名前缀
-exports.urlBase = '643d105cee39e30001732fdb'
+exports.urlBase = '6437cab4291ee50001318391'
 // 时间信息
-exports.saveTime = '2023-12-08 12:00-16:00'
+exports.saveTime = '2023-12-08 14:00-20:00'
 //reservationConfigId 在local的 reserveId
-exports.reservationConfigId = '656d640a5df9bc0001f3bbf9'
+exports.reservationConfigId = '656fed6c5df9bc0001f6ae6a'
