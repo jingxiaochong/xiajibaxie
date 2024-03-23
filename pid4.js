306ac29fff67eb8fd207f6582baa19e0d20746bf
jingxc <jingxiaochong@HUAWEI-MacBook-Pro.local>
2024-03-23 14:14:23 +0800

    信息

---
+++ pid4.js
@@ -0,0 +1,57 @@
+const axios = require('axios')
+const info = require('./info4.js')
+const public = require('./public.js')
+
+start()
+function start() {
+  let date = new Date()
+  if (date.getHours() == 13 && date.getMinutes() == 59 && date.getSeconds() == 55) {
+    postData()
+  } else {
+    setTimeout(() => {
+      start()
+    }, 1);
+  }
+}
+
+
+// 往死里递归
+function postData() {
+  postFunction(info.postData1,info.AccessToken1)
+  postFunction(info.postData1,info.AccessToken2)
+  postFunction(info.postData1,info.AccessToken3)
+  postFunction(info.postData1,info.AccessToken4)
+  let date = new Date()
+  if (date.getHours() == 14 && date.getMinutes() == 0 && date.getSeconds() == 20) {
+    return
+  } else {
+    setTimeout(() => {
+      postData()
+    }, 100);
+  }
+}
+
+
+function postFunction(info,token) {
+  let time = public.saveTime
+  let data = {
+    "reservationConfigId": public.reservationConfigId,
+    "reservationDate":time.data,
+    "startTime": time.startTime,
+    "endTime": time.endTime,
+    "showOrderId": "",
+    "showSessionId": "",
+    "reservationAudienceParams": info,
+    "src": "H5"
+  }
+  axios.post(public.postUrl, data,
+    {
+      "headers": {
+        "access-token": token,
+      }
+    }).then((res) => {
+      console.log(res.data);
+    }).catch((err) => {
+      console.log(err);
+    });
+}
