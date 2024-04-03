dc625cde5997ba46d9d277258cca094123fbb84f
jingxc <jingxiaochong@HUAWEI-MacBook-Pro.local>
2024-04-03 17:57:54 +0800

    sql

--- mysql.js
+++ mysql.js
@@ -2,16 +2,66 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root', 
+  user: 'root',
   password: 'jxc123456',
-  charset  : 'utf8mb4',
-  // database: 'your_database'
+  charset: 'utf8',
+  database: 'info'
 })
 
-connection.connect((err) => {
-  console.log(err);
-})
+connection.connect(function (err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+});
 
-// connection.query('SELECT * FROM your_table', (err, results) => {
-  
+
+function createTable(params) {
+  let createTodos = `create table if not exists tokens(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    access_token varchar(21840)
+  );
+  `;
+
+  connection.query(createTodos, function (err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+}
+
+
+
+function deleteTable() {
+  // 定义要删除的表格名称
+  const tableName = 'tokens';
+  // 构建删除表格的SQL语句
+  const dropTableQuery = `DROP TABLE IF EXISTS ${tableName}`;
+  // 执行SQL语句删除表格
+  connection.query(dropTableQuery, (err, results, fields) => {
+    if (err) {
+      console.error('Error dropping table: ' + err.message);
+      return;
+    }
+    console.log('Table dropped successfully');
+  });
+}
+
+// createTable()
+
+// connection.query('INSERT INTO tokens (id, access_token) VALUES (1, "token")', function (err, results, fields) {
+//   if (err) {
+//     console.log(err.message);
+//     return
+//   }
+//   console.log('success');
 // })
+
+
+// connection.end(function (err) {
+//   if (err) {
+//     return console.log(err.message);
+//   }
+// });
+
+
+
