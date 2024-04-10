df0f15d1cea1e32a15bfaefd33fb9a8583cb9c4f
jingxc <jingxiaochong@HUAWEI-MacBook-Pro.local>
2024-04-10 16:47:10 +0800

    111

--- public.js
+++ public.js
@@ -1,4 +1,13 @@
 const mysql = require('mysql');
+exports.urlBase = ''
+exports.saveTime = {
+  data: '',
+  startTime: "",
+  endTime: ""
+}
+exports.reservationConfigId = ''
+
+
 const connection = mysql.createConnection({
   host: '116.62.122.121',
   port: '3306',
@@ -11,29 +20,22 @@ connection.query(`SELECT * FROM active_info;`, (error, results, fields) => {
   if (error) {
     console.log(error);
   }
-
+  exports.urlBase = results[0].urlBase
+  exports.saveTime.data = results[0].data
+  exports.saveTime.startTime = results[0].startTime
+  exports.saveTime.endTime = results[0].endTime
+  exports.reservationConfigId = results[0].reservationConfigId
+  // 参数
+  exports.Referer = `https://${exports.urlBase}.caiyicloud.com/reserve/reserve-detail/personinfo`
+  // 查询接口
+  exports.searchURl = `https://${exports.urlBase}.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_configs/${exports.reservationConfigId}/instance`
+  // 请求接口
+  exports.postUrl = `https://${exports.urlBase}.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_orders?channelId=&terminalSrc=H5`
 })
+
+
 connection.end(function (err) {
   if (err) {
     return console.log(err.message);
   }
 });
-//活动域名前缀
-exports.urlBase = '6437cab4291ee50001318391'
-// 时间信息
-exports.saveTime = {
-  data:'1712764800000',
-  startTime:"14:00",
-  endTime:"20:00"
-}
-//reservationConfigId 在local的 reserveId
-exports.reservationConfigId = '66126d1314f60000011f8b02'
-
-// 参数
-exports.Referer = `https://${exports.urlBase}.caiyicloud.com/reserve/reserve-detail/personinfo`
-
-// 查询接口
-exports.searchURl = `https://${exports.urlBase}.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_configs/${exports.reservationConfigId}/instance`
-
-// 请求接口
-exports.postUrl = `https://${exports.urlBase}.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_orders?channelId=&terminalSrc=H5`
