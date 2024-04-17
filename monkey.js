62ebd91fa44964014fde2ccfc73e3f9f4e547012
jingxc <jingxiaochong@HUAWEI-MacBook-Pro.local>
2024-04-17 15:39:34 +0800

    monkey

--- monkey.js
+++ monkey.js
@@ -8,48 +8,50 @@
 // ==/UserScript==
 
 'use strict';
-let flag = true;
-window.onload = function(){
-      if(flag){
-          searchBtn()
+window.onload = function () {
+    let arr = window.location.href.split('/')
+    console.log(arr[arr.length - 1]);
+    if (arr[arr.length - 1]  == 'personinfo') {
+        searchPerson()
+    } else {
+        searchBtn()
+    }
+
+};
+function searchBtn() {
+    let btn = document.querySelector('.confirm-btn');
+    if (btn) {
+        // btn.textContent == '所选日期将于17日14点00分开放预约'
+        if (btn.textContent != '确定') {
+            setTimeout(() => {
+                window.location.reload(true)
+            }, 100)
+        } else {
+            btn.click()
+        }
+
+    } else {
+        setTimeout(() => {
+            searchBtn();
+        }, 1)
+    }
+};
+function searchPerson() {
+      if(document.querySelector('.add-person')){
+        document.querySelector('.add-person').click()
+          setTimeout(()=>{
+              let evt = document.createEvent('HTMLEvents');
+              evt.initEvent('input', true, true);
+
+              document.getElementsByClassName('view_form-input')[0].value = '翁蹿腾';
+              document.getElementsByClassName('view_form-input')[0].dispatchEvent(evt)
+
+              document.getElementsByClassName('view_form-input')[2].value = '330283199807066019';
+              document.getElementsByClassName('view_form-input')[2].dispatchEvent(evt)
+
+              document.querySelector('.person-confirm').click()},100)
+
       }else{
           searchPerson()
       }
-  };
-function searchBtn(){
-  let btn = document.querySelector('.confirm-btn');
-  if(btn){
-      console.log(btn.textContent)
-      if(btn.textContent == '所选日期将于17日14点00分开放预约'){
-          setTimeout(()=>{
-              window.location.reload(true)
-          },100)
-      }else{
-          flag = false;
-          btn.click()
-      }
-
-  }else{
-      setTimeout(()=>{
-          searchBtn();
-      },1)
-  }
-  };
-function searchPerson(){
-  if(document.querySelector('.add-person')){
-      document.getElementsByClassName('add-person')[0].click()
-      setTimeout(()=>{
-          let evt = document.createEvent('HTMLEvents');
-          evt.initEvent('input', true, true);
-
-          document.getElementsByClassName('view_form-input')[0].value = '井晓冲';
-          document.getElementsByClassName('view_form-input')[0].dispatchEvent(evt)
-
-          document.getElementsByClassName('view_form-input')[2].value = '411121199808210015';
-          document.getElementsByClassName('view_form-input')[2].dispatchEvent(evt)
-
-          document.querySelector('.person-confirm').click()})
-  }else{
-      searchPerson()
-  }
 }
