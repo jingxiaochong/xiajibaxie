2529760bbff253fe21b4b911b648bdbd2179bc31
jingxiaochong <1142281477@qq.com>
2024-04-27 13:32:39 +0800

    pub

--- public.js
+++ public.js
@@ -11,7 +11,7 @@ exports.reservationConfigId = '66221da421954b000139ed43'
 // 参数
 exports.Referer = 'https://6437cab4291ee50001318391.caiyicloud.com/reserve/reserve-detail/personinfo'
 // 查询接口
-exports.searchURl = 'https://6437cab4291ee50001318391.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_configs/undefined/instance'
+exports.searchURl = 'https://6437cab4291ee50001318391.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_configs/66221da421954b000139ed43/instance'
 // 请求接口
 exports.postUrl = 'https://6437cab4291ee50001318391.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_orders?channelId=&terminalSrc=H5'
 
