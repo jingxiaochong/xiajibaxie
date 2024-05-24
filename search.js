80ff51127e91efea4efb6f4d730ea48cbbd8b14c
jingxc <jingxiaochong@HUAWEI-MacBook-Pro.local>
2024-05-24 10:21:48 +0800

    search保守 0激进

--- search.js
+++ search.js
@@ -9,7 +9,7 @@ axios.get('http://116.62.122.121:4396/getInfo').then((res) => {
             "audienceIdentityNumber": res.data.card_id,
             "audienceIdentityType": "ID_CARD",
             "audienceName": res.data.user,
-            "audienceCellphone": null,
+            "audienceCellphone": Math.random() >= 0.3 ? null : res.data.phone,
             "seatInfo": "",
             "showOrderTicketItemId": ""
         }]
