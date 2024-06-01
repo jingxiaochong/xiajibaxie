5e90cac70c216565f55e64db6b19983394cef071
jingxiaochong <1142281477@qq.com>
2024-06-01 08:54:56 +0800

    public11

--- public.js
+++ public.js
@@ -1,19 +1,19 @@
 
   const mysql = require('mysql');
-  exports.urlBase = '6437cab4291ee50001318391'
+  exports.urlBase = '6566d890783b53000119b89d'
   exports.saveTime = {
     data: '1717171200000',
-    startTime: "14:00",
-    endTime: "20:00"
+    startTime: "19:30",
+    endTime: "21:30"
 }
-exports.reservationConfigId = '66580dad816b6d000127d741'
+exports.reservationConfigId = '665707b7887e5a00011576fd'
 
 // 参数
-exports.Referer = 'https://6437cab4291ee50001318391.caiyicloud.com/reserve/reserve-detail/personinfo'
+exports.Referer = 'https://6566d890783b53000119b89d.caiyicloud.com/reserve/reserve-detail/personinfo'
 // 查询接口
-exports.searchURl = 'https://6437cab4291ee50001318391.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_configs/66580dad816b6d000127d741/instance'
+exports.searchURl = 'https://6566d890783b53000119b89d.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_configs/665707b7887e5a00011576fd/instance'
 // 请求接口
-exports.postUrl = 'https://6437cab4291ee50001318391.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_orders?channelId=&terminalSrc=H5'
+exports.postUrl = 'https://6566d890783b53000119b89d.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_orders?channelId=&terminalSrc=H5'
 // 查询顺序接口
-exports.searchOrder = 'https://6437cab4291ee50001318391.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_orders/id?src=WEB&channelId=&terminalSrc=WEB'
+exports.searchOrder = 'https://6566d890783b53000119b89d.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_orders/id?src=WEB&channelId=&terminalSrc=WEB'
 
