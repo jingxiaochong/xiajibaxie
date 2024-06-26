6aac46be415e78d81cfd6e40e789f2536fd3878f
jingxc <jingxiaochong@HUAWEI-MacBook-Pro.local>
2024-06-26 16:27:51 +0800

    test

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ let public = {}
 let userInfo = {}
 axios.get('http://116.62.122.121:4396/getInfo').then((infores) => {
     userInfo = {
-        // token: infores.data.access_token,
         token: infores.data.refresh_token,
         info: [{
             "audienceIdentityNumber": infores.data.card_id,
@@ -37,6 +36,7 @@ axios.get('http://116.62.122.121:4396/getInfo').then((infores) => {
 start()
 function start() {
     let date = new Date()
+    console.log(date.getHours(),date.getMinutes(),date.getSeconds())
     if (date.getHours() == 14 && date.getMinutes() == 59 && date.getSeconds() == 55) {
         setInterval(() => {
             search()
@@ -49,63 +49,4 @@ function start() {
             start()
         }, 1);
     }
-}
-
-function search() {
-    axios.get(public.searchURl, {
-        "headers": {
-            "access-token": userInfo.token,
-        }
-    }).then(res => {
-        if (res.data.data.reservationDates[0].configItems[0].isOnsale) {
-            for (let index = 0; index < 1000; index++) {
-                postFunction()
-            }
-        }
-    })
-}
-
-function postFunction() {
-    let data = {
-        "reservationConfigId": public.reservationConfigId,
-        "reservationDate": public.saveTime.date,
-        "startTime": public.saveTime.startTime,
-        "endTime": public.saveTime.endTime,
-        "showOrderId": "",
-        "showSessionId": "",
-        "reservationAudienceParams": userInfo.info,
-        "src": "H5"
-    }
-    axios.post(public.postUrl, data,
-        {
-            "headers": {
-                "access-token": userInfo.token,
-            }
-        }).then((res) => {
-            console.log(res.data);
-            if (res.data.statusCode == 200 && res.data.data.id) {
-                axios.post('http://116.62.122.121:4396/putUserInfo', {
-                    data: {
-                        token: userInfo.token,
-                        id: res.data.data.id
-                    }
-                })
-                setTimeout(() => {
-                    // 根据成功id拿到顺序
-                    axios.get(public.searchOrder.replace('id', res.data.data.id), {
-                        headers: {
-                            "access-token": userInfo.token,
-                        }
-                    }).then((success) => {
-                        // 获取到成功序列号 插入
-                        axios.post('http://116.62.122.121:4396/editOrders', {
-                            id: res.data.data.id,
-                            order: success.data.data.orderItems[0].reservationSequence,
-                            token: userInfo.token
-                        })
-                    })
-                }, 10000);
-
-            }
-        })
 }
