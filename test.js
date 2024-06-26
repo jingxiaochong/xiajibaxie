d84f8745130ddc3f340bba278047404aad4823b0
jingxc <jingxiaochong@HUAWEI-MacBook-Pro.local>
2024-06-26 16:31:36 +0800

    test time

--- test.js
+++ test.js
@@ -2,41 +2,56 @@ const axios = require('axios')
 let public = {}
 
 let userInfo = {}
-axios.get('http://116.62.122.121:4396/getInfo').then((infores) => {
-    userInfo = {
-        token: infores.data.refresh_token,
-        info: [{
-            "audienceIdentityNumber": infores.data.card_id,
-            "audienceIdentityType": "ID_CARD",
-            "audienceName": infores.data.user,
-            "audienceCellphone": null,
-            "seatInfo": "",
-            "showOrderTicketItemId": ""
-        }]
-    }
+// axios.get('http://116.62.122.121:4396/getInfo').then((infores) => {
+//     userInfo = {
+//         token: infores.data.refresh_token,
+//         info: [{
+//             "audienceIdentityNumber": infores.data.card_id,
+//             "audienceIdentityType": "ID_CARD",
+//             "audienceName": infores.data.user,
+//             "audienceCellphone": null,
+//             "seatInfo": "",
+//             "showOrderTicketItemId": ""
+//         }]
+//     }
 
-    public.searchURl = `https://${infores.data.base_url}.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_configs/${infores.data.active_id}/instance`
-    public.postUrl = `https://${infores.data.base_url}.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_orders`
-    public.searchOrder = `https://${infores.data.base_url}.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_orders/id`
-    public.reservationConfigId = infores.data.active_id
-    axios.get(public.searchURl, {
-        "headers": {
-            "access-token": userInfo.token,
-        }
-    }).then(res => {
-        console.log(res.data);
-        public.saveTime = {
-            date: res.data.data.reservationDates[0].reservationDate,
-            startTime: res.data.data.reservationDates[0].configItems[0].configTimeItems[0].startTime,
-            endTime: res.data.data.reservationDates[0].configItems[0].configTimeItems[0].endTime,
-        }
-    })
-})
+//     public.searchURl = `https://${infores.data.base_url}.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_configs/${infores.data.active_id}/instance`
+//     public.postUrl = `https://${infores.data.base_url}.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_orders`
+//     public.searchOrder = `https://${infores.data.base_url}.caiyicloud.com/cyy_buyerapi/buyer/cyy/v1/reservation_orders/id`
+//     public.reservationConfigId = infores.data.active_id
+//     axios.get(public.searchURl, {
+//         "headers": {
+//             "access-token": userInfo.token,
+//         }
+//     }).then(res => {
+//         console.log(res.data);
+//         public.saveTime = {
+//             date: res.data.data.reservationDates[0].reservationDate,
+//             startTime: res.data.data.reservationDates[0].configItems[0].configTimeItems[0].startTime,
+//             endTime: res.data.data.reservationDates[0].configItems[0].configTimeItems[0].endTime,
+//         }
+//     })
+// })
 
 start()
 function start() {
     let date = new Date()
-    console.log(date.getHours(),date.getMinutes(),date.getSeconds())
+    const options = {
+      timeZone: 'Asia/Shanghai',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    };
+    const formatter = new Intl.DateTimeFormat('en-US', options);
+    const parts = formatter.formatToParts(date);
+    let hour, minute, second;
+    for (const part of parts) {
+      if (part.type === 'hour') hour = part.value;
+      if (part.type === 'minute') minute = part.value;
+      if (part.type === 'second') second = part.value;
+    }
+    console.log(hour, minute, second)
     if (date.getHours() == 14 && date.getMinutes() == 59 && date.getSeconds() == 55) {
         setInterval(() => {
             search()
