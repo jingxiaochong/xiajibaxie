0f02e0be0a1952a5108eba250603d6382b8e5b46
unknown <1142281477@qq.com>
2024-06-27 22:46:56 +0800

    400

--- test.js
+++ test.js
@@ -35,7 +35,7 @@ axios.get('http://116.62.122.121:4396/getInfo?type=1').then((infores) => {
         }
         setInterval(() => {
             postFunction()
-        }, 300);
+        }, 400);
     })
 })
 
