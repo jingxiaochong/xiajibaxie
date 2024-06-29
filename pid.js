98da17f78f1f962e1255e0c675eac213639c2cb1
jingxc <jingxiaochong@HUAWEI-MacBook-Pro.local>
2024-06-29 12:17:35 +0800

    500

--- pid.js
+++ pid.js
@@ -9,9 +9,9 @@ let public = {}
 // }
 
 let userInfo = {}
-// let num = 0
+let num = 500
   // num = infores.data.num
-let num =  Math.floor(Math.random() * (1001)) //随机启动时间
+// let num =  Math.floor(Math.random() * (1001)) //随机启动时间
 let timer =  Math.floor(Math.random() * 1699) + 300; //随机间隔时间
 axios.get('http://116.62.122.121:4396/getInfo?activeId=667c1b471884c80001e58916').then((infores) => {
   console.log(infores.data.user);
